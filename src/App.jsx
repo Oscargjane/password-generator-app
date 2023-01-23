8f49b491acf16f874d95a1d097eac0f0f8d49ea7
Oscar Jané <oscar.gjane@gmail.com>
2023-01-23 20:02:39 +0100

    refactor: refactored main component that renders the app in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import Theme from './components/styles/Theme';
 import GlobalStyle from './components/styles/Global';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import PasswordGenerated from './components/Password/PasswordGenerated';
-import { StyledContainer } from './components/styles/Container.styled';
-import { StyledButton } from './components/styles/Button.styled';
-import PasswordControls from './components/Password/PasswordControls';
+import PasswordGenerator from './components/Password/PasswordGenerator';
 
 function App() {
   return (
     <Theme>
       <GlobalStyle />
-      <StyledContainer>
-        <h1>Password Generator</h1>
-        <PasswordGenerated />
-        <PasswordControls />
-        <StyledButton>
-          Generate
-          <FontAwesomeIcon icon={faArrowRight} />
-        </StyledButton>
-      </StyledContainer>
+      <PasswordGenerator />
     </Theme>
   );
 }
